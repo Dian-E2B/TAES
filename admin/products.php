1053380baf85f1d9c802ebe9477ecfe26c3beef3
DESKTOP-3IQTVPL\Diana <d.emuy.464149@umindanao.edu.ph>
2024-07-17 21:22:55 +0800

    Product status updated

--- admin/products.php
+++ admin/products.php
@@ -6,11 +6,10 @@ require '../application/controllers/admin/add-product.php';
 
 if (isset($_POST['delete-product'])) {
 
-  $data = ['id' => $_GET['id']];
+    $data = ['id' => $_GET['id']];
 
-  $query = "DELETE FROM products WHERE id = :id";
-  $function->delete($query, $data);
-
+    $query = "UPDATE products SET product_status = 0 WHERE id = :id";
+    $function->delete($query, $data);
 }
 
 if (isset($_POST['edit-product'])) {
@@ -23,20 +22,20 @@ if (isset($_POST['edit-product'])) {
 
     $directory = '../images/products/';
     $path = $directory . $photo;
-    $target_file = $directory.basename($photo);
+    $target_file = $directory . basename($photo);
     $FileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
     $allowed = array('jpeg', 'png', 'jpg');
     $filename = $photo;
 
-    if (empty($photo)){
-		$photo = $product['photo'];
+    if (empty($photo)) {
+        $photo = $product['photo'];
     } else {
         $extension = pathinfo($filename, PATHINFO_EXTENSION);
-        if(!in_array($extension, $allowed)) {
-          echo 'Invalid file type.';
+        if (!in_array($extension, $allowed)) {
+            echo 'Invalid file type.';
         } else {
-          move_uploaded_file( $_FILES['photo']['tmp_name'], $path); 
+            move_uploaded_file($_FILES['photo']['tmp_name'], $path);
         }
     }
 
@@ -50,12 +49,12 @@ if (isset($_POST['edit-product'])) {
         'photo' => $photo,
         'id' => $id
     ];
-  
+
     $query = "UPDATE products SET name = :name, description = :description, price = :price, 
             QuantityInStock = :quantity, category_id = :category_id, supplier_id = :supplier_id, 
             photo = :photo WHERE id = :id";
     $function->update($query, $data);
-  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -102,8 +101,7 @@ if (isset($_POST['edit-product'])) {
                         </div>
                         <div class="body">
                             <div class="table-responsive">
-                                <table class="table table-bordered table-striped table-hover dataTable js-exportable"
-                                    id="products-table">
+                                <table class="table table-bordered table-striped table-hover dataTable js-exportable" id="products-table">
                                     <thead>
                                         <tr>
                                             <th>ID</th>
@@ -121,58 +119,49 @@ if (isset($_POST['edit-product'])) {
                                     <tbody>
 
                                         <?php
-                                    try {
+                                        try {
 
-                                        $query = "SELECT products.id, products.photo, products.name, products.price, products.QuantityInStock, products.QuantitySold, supplier.name as 'supplier_name', category.name as 'category_name' FROM products INNER JOIN category ON products.category_id = category.id INNER JOIN supplier ON products.supplier_id = supplier.id";
-                                        $rows = $function->selectAll($query);
-                                        foreach ($rows as $row) { ?>
+                                            $query = "SELECT products.id, products.photo, products.name, products.price, products.QuantityInStock, products.QuantitySold, supplier.name as 'supplier_name', category.name as 'category_name' FROM products INNER JOIN category ON products.category_id = category.id INNER JOIN supplier ON products.supplier_id = supplier.id WHERE products.product_status = 1";
+                                            $rows = $function->selectAll($query);
+                                            foreach ($rows as $row) { ?>
 
-                                        <tr>
-                                            <td><?php echo $row['id']; ?></td>
-                                            <td class="text-center">
-                                                <img src="../images/products/<?php echo $row['photo']; ?>" width="70"
-                                                    height="70" />
-                                            </td>
-                                            <td><?php echo strlen($row['name']) > 15 ? substr($row['name'],0,15) . "..." : $row['name']; ?></td>
-                                            <td>PHP <?php echo number_format($row['price'], 2); ?></td>
-                                            <td>
-                                                <span
-                                                    class="label bg-<?php echo ($row['QuantityInStock']<=30) ? 'red' : 'green'; ?>">
-                                                    <?php echo $row['QuantityInStock']; ?>
-                                                </span>
-                                            </td>
-                                            <td><?php echo $row['QuantitySold']; ?></td>
-                                            <td><?php echo $row['supplier_name']; ?></td>
-                                            <td><?php echo $row['category_name']; ?></td>
-                                            <td class="text-center js-sweetalert">
-                                                <button data-toggle="modal"
-                                                    data-target="#infoModal_<?php echo $row['id']; ?>"
-                                                    class="btn btn-info btn-xs waves-effect">
-                                                    <i class="material-icons" style="font-size:1.6rem;"
-                                                        data-toggle="modal" data-target="#infoModal">info_outline</i>
-                                                </button>
-                                                <button data-toggle="modal"
-                                                    data-target="#editModal_<?php echo $row['id']; ?>"
-                                                    class="btn btn-warning btn-xs waves-effect">
-                                                    <i class="material-icons" style="font-size:1.6rem;">mode_edit</i>
-                                                </button>
-                                                <button type="button" class="btn btn-danger btn-xs waves-effect"
-                                                    data-toggle="modal"
-                                                    data-target="#deleteModal_<?php echo $row['id']; ?>">
-                                                    <i class="material-icons" style="font-size:1.6rem;">delete</i>
-                                                </button>
+                                                <tr>
+                                                    <td><?php echo $row['id']; ?></td>
+                                                    <td class="text-center">
+                                                        <img src="../images/products/<?php echo $row['photo']; ?>" width="70" height="70" />
+                                                    </td>
+                                                    <td><?php echo strlen($row['name']) > 15 ? substr($row['name'], 0, 15) . "..." : $row['name']; ?></td>
+                                                    <td>PHP <?php echo number_format($row['price'], 2); ?></td>
+                                                    <td>
+                                                        <span class="label bg-<?php echo ($row['QuantityInStock'] <= 30) ? 'red' : 'green'; ?>">
+                                                            <?php echo $row['QuantityInStock']; ?>
+                                                        </span>
+                                                    </td>
+                                                    <td><?php echo $row['QuantitySold']; ?></td>
+                                                    <td><?php echo $row['supplier_name']; ?></td>
+                                                    <td><?php echo $row['category_name']; ?></td>
+                                                    <td class="text-center js-sweetalert">
+                                                        <button data-toggle="modal" data-target="#infoModal_<?php echo $row['id']; ?>" class="btn btn-info btn-xs waves-effect">
+                                                            <i class="material-icons" style="font-size:1.6rem;" data-toggle="modal" data-target="#infoModal">info_outline</i>
+                                                        </button>
+                                                        <button data-toggle="modal" data-target="#editModal_<?php echo $row['id']; ?>" class="btn btn-warning btn-xs waves-effect">
+                                                            <i class="material-icons" style="font-size:1.6rem;">mode_edit</i>
+                                                        </button>
+                                                        <button type="button" class="btn btn-danger btn-xs waves-effect" data-toggle="modal" data-target="#deleteModal_<?php echo $row['id']; ?>">
+                                                            <i class="material-icons" style="font-size:1.6rem;">delete</i>
+                                                        </button>
 
-                                                <?php include 'product-modal.php'; ?>
-                                            </td>
-                                        </tr>
+                                                        <?php include 'product-modal.php'; ?>
+                                                    </td>
+                                                </tr>
 
                                         <?php
+                                            }
+                                        } catch (PDOException $e) {
+                                            echo "There is some problem in connection: " . $e->getMessage();
                                         }
-                                    } catch (PDOException $e) {
-                                        echo "There is some problem in connection: " . $e->getMessage();
-                                    }
 
-                                    ?>
+                                        ?>
 
                                     </tbody>
                                 </table>
@@ -197,16 +186,13 @@ if (isset($_POST['edit-product'])) {
                         <label class="form-label">Name</label>
                         <div class="form-group">
                             <div class="form-line">
-                                <input type="text" id="name" name="name" placeholder="Enter product name"
-                                    class="form-control" required>
+                                <input type="text" id="name" name="name" placeholder="Enter product name" class="form-control" required>
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="form-label">Description</label>
                             <div class="form-line">
-                                <textarea rows="1" name="description" placeholder="Enter description"
-                                    class="form-control no-resize auto-growth"
-                                    style="overflow: hidden; overflow-wrap: break-word; height: 35px;" required></textarea>
+                                <textarea rows="1" name="description" placeholder="Enter description" class="form-control no-resize auto-growth" style="overflow: hidden; overflow-wrap: break-word; height: 35px;" required></textarea>
                             </div>
                         </div>
                         <div class="form-group">
@@ -218,20 +204,19 @@ if (isset($_POST['edit-product'])) {
                         <div class="form-group">
                             <label class="form-label">Quantity</label>
                             <div class="form-line">
-                                <input type="text" name="quantity" placeholder="Enter quantity"
-                                    class="form-control" required>
+                                <input type="text" name="quantity" placeholder="Enter quantity" class="form-control" required>
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="form-label">Category</label>
                             <select name="category" class="form-control show-tick">
                                 <?php
-                                    $query = "SELECT * FROM category";
-                                    $rows = $function->selectAll($query);
-                                    foreach ($rows as $row): ?>
-                                <option value="<?php echo $row['id']; ?>">
-                                    <?php echo $row['name']; ?>
-                                </option>
+                                $query = "SELECT * FROM category";
+                                $rows = $function->selectAll($query);
+                                foreach ($rows as $row) : ?>
+                                    <option value="<?php echo $row['id']; ?>">
+                                        <?php echo $row['name']; ?>
+                                    </option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
@@ -239,12 +224,12 @@ if (isset($_POST['edit-product'])) {
                             <label class="form-label">Supplier</label>
                             <select name="supplier" class="form-control show-tick">
                                 <?php
-                                    $query = "SELECT * FROM supplier";
-                                    $rows = $function->selectAll($query);
-                                    foreach ($rows as $row): ?>
-                                <option value="<?php echo $row['id']; ?>">
-                                    <?php echo $row['name']; ?>
-                                </option>
+                                $query = "SELECT * FROM supplier";
+                                $rows = $function->selectAll($query);
+                                foreach ($rows as $row) : ?>
+                                    <option value="<?php echo $row['id']; ?>">
+                                        <?php echo $row['name']; ?>
+                                    </option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
